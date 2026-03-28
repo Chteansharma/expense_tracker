26e37bf0106b4471ac5a40f124a72904fdd65a67
CHETAN SHARMA <136621054+Chteansharma@users.noreply.github.com>
2026-03-28 13:37:08 +0530

    server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: https://expense-tracker-gamma-lac.vercel.app,
+    origin: "https://expense-tracker-gamma-lac.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
